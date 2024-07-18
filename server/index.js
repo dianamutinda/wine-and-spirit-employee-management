764ed2b447ae3d4864cbccf5d22468c7963890d6
diana mutinda <jaydiana25@gmail.com>
2024-07-18 11:55:06 +0300

    creates the users register route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import userRoutes from './routes/users.routes.js';
 
 const app = express();
+app.use("/api/users", userRoutes)
 
 app.listen(3000, (res, req) => {
     console.log('Server is running on port 3000')
